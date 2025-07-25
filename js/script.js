aeb67da4bffd88b4160c9009466c419472fe6064
JacksonWeudes <weudes2024jackson@gmail.com>
2025-07-25 16:08:50 -0300

    estrutura finalizada

--- js/script.js
+++ js/script.js
@@ -14,11 +14,16 @@ window.addEventListener("scroll", () => {
 const menuBtn = document.getElementById("mbtn")
 const closeBtn = document.getElementById("cbtn")
 const sideMenu = document.getElementById("smenu")
+const overlay = document.getElementById("overlay")
 
 menuBtn.addEventListener("click", () => {
     sideMenu.classList.add("active")
+    overlay.classList.add("active")
+    document.body.classList.add("active")
 })
 
 closeBtn.addEventListener("click", () => {
     sideMenu.classList.remove("active")
+    overlay.classList.remove("active")
+    document.body.classList.remove("active")
 })
